655fca2fd9e7ea336dafdb6b7489f9249c0827ce
Justin Garcia <functor@duck.com>
2022-08-11 13:56:22 +0800

    Better helpers for hyrule

--- src/Always/Hyrule.purs
+++ src/Always/Hyrule.purs
@@ -2,13 +2,14 @@ module Always.Hyrule where
 
 import Prelude
 
-import Always (Always, always, unalways, yes, yesn't)
+import Always (Always, AlwaysProof(..), always, unalways, yes)
+import Data.Leibniz (coerce, symm)
 import Data.Profunctor (dimap)
 import FRP.Event (AnEvent)
 import Unsafe.Coerce (unsafeCoerce)
 
-alwaysEventYes :: forall m. Monad m => AnEvent m ~> AnEvent (Always m)
-alwaysEventYes = unsafeUnwrapEvent >>> dimap (map (unalways yes)) (map always <<< always) >>>
+alwaysEvent :: forall m. Monad m => AnEvent m ~> AnEvent (Always m)
+alwaysEvent = unsafeUnwrapEvent >>> dimap (map (unalways yes)) (map always <<< always) >>>
   unsafeWrapEvent
   where
   unsafeUnwrapEvent
@@ -19,43 +20,19 @@ alwaysEventYes = unsafeUnwrapEvent >>> dimap (map (unalways yes)) (map always <<
     :: forall a. ((a -> Always m Unit) -> Always m (Always m Unit)) -> AnEvent (Always m) a
   unsafeWrapEvent = unsafeCoerce
 
-alwaysEventYesn't :: forall m n. Monad m => Monad n => AnEvent m ~> AnEvent (Always n)
-alwaysEventYesn't = unsafeUnwrapEvent >>> dimap (map (unalways yesn't)) mempty >>>
-  unsafeWrapEvent
+unalwaysEvent :: forall m n. Monad m => Monad n => AlwaysProof m n -> AnEvent (Always m) ~> AnEvent n
+unalwaysEvent proof = unsafeUnwrapEvent >>> dimap asAlways asBase >>> unsafeWrapEvent
   where
-  unsafeUnwrapEvent
-    :: forall a. AnEvent m a -> ((a -> m Unit) -> m (m Unit))
-  unsafeUnwrapEvent = unsafeCoerce
+  asAlways :: forall a. (a -> n Unit) -> a -> Always m Unit
+  asAlways = case proof of
+    Yes prf -> map (always <<< coerce (symm prf))
+    Yesn't -> mempty
 
-  unsafeWrapEvent
-    :: forall a. ((a -> Always n Unit) -> Always n (Always n Unit)) -> AnEvent (Always n) a
-  unsafeWrapEvent = unsafeCoerce
+  asBase :: Always m (Always m Unit) -> n (n Unit)
+  asBase = case proof of
+    Yes _ -> map (unalways proof) <<< unalways proof
+    Yesn't -> \_ -> pure $ pure unit
 
-unalwaysEventYes
-  :: forall m
-   . Monad m
-  => AnEvent (Always m) ~> AnEvent m
-unalwaysEventYes = unsafeUnwrapEvent
-  >>> dimap (map always) (map (unalways yes) <<< unalways yes)
-  >>> unsafeWrapEvent
-  where
-  unsafeUnwrapEvent
-    :: forall a. AnEvent (Always m) a -> ((a -> Always m Unit) -> Always m (Always m Unit))
-  unsafeUnwrapEvent = unsafeCoerce
-
-  unsafeWrapEvent
-    :: forall a. ((a -> m Unit) -> m (m Unit)) -> AnEvent m a
-  unsafeWrapEvent = unsafeCoerce
-
-unalwaysEventYesn't
-  :: forall m n
-   . Monad m
-  => Monad n
-  => AnEvent (Always m) ~> AnEvent n
-unalwaysEventYesn't = unsafeUnwrapEvent
-  >>> dimap (map (\_ -> pure mempty)) (map (unalways yesn't) <<< unalways yesn't)
-  >>> unsafeWrapEvent
-  where
   unsafeUnwrapEvent
     :: forall a. AnEvent (Always m) a -> ((a -> Always m Unit) -> Always m (Always m Unit))
   unsafeUnwrapEvent = unsafeCoerce
--- test/Main.purs
+++ test/Main.purs
@@ -2,10 +2,11 @@ module Test.Main where
 
 import Prelude
 
-import Always (always, unalways, yesn't)
-import Always.Hyrule (alwaysEventYesn't, unalwaysEventYesn't)
+import Always (Always, always, unalways, yes, yesn't)
+import Always.Hyrule (alwaysEvent, unalwaysEvent)
 import Control.Monad.ST (ST)
-import Control.Monad.ST.Global (toEffect)
+import Control.Monad.ST.Global (Global, toEffect)
+import Control.Monad.ST.Ref as STRef
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
@@ -25,11 +26,34 @@ main = launchAff_ $ runSpec [ consoleReporter ] do
         void $ Ref.write true condition
       result <- Ref.read condition
       result `shouldEqual` false
-  describe "Always.Hyrule" do
-    it "nullifies an Event" $ liftEffect do
+    it "restores an Effect" $ liftEffect do
       condition <- Ref.new false
-      let (stEvent :: AnEvent (_ (ST _)) _) = alwaysEventYesn't (bang unit :: AnEvent Effect Unit)
-      _ <- subscribe (unalwaysEventYesn't stEvent) \_ ->
+      _ <- unalways yes $ always do
+        void $ Ref.write true condition
+      result <- Ref.read condition
+      result `shouldEqual` true
+  describe "Always.Hyrule" do
+    it "nullifies an Effect" $ liftEffect do
+      condition <- toEffect $ STRef.new false
+      let
+        alEvent :: AnEvent (Always Effect) Unit
+        alEvent = alwaysEvent $ bang unit
+
+        alEvent' :: AnEvent (ST Global) Unit
+        alEvent' = unalwaysEvent yesn't alEvent
+      _ <- toEffect $ subscribe alEvent' \_ -> do
+             void $ STRef.write true condition
+      result <- toEffect $ STRef.read condition
+      result `shouldEqual` false
+    it "restores an Effect" $ liftEffect do
+      condition <- Ref.new false
+      let
+        alEvent :: AnEvent (Always Effect) Unit
+        alEvent = alwaysEvent $ bang unit
+
+        alEvent' :: AnEvent Effect Unit
+        alEvent' = unalwaysEvent yes alEvent
+      _ <- subscribe alEvent' \_ -> do
              void $ Ref.write true condition
       result <- Ref.read condition
-      result `shouldEqual` false
+      result `shouldEqual` true
